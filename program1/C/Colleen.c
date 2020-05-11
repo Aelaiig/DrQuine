5f44c99645d601754855ce0c289a215877e4febb
Aelaïg Weiler <aelaig.weiler@teamwork.net>
2020-05-11 09:51:30 +0200

    send file to get header from guacamole

---
+++ program1/C/Colleen.c
@@ -0,0 +1,46 @@
+// 42 header
+// 42 header
+
+#include <stdio.h>
+
+/*
+    This is the first program of Dr Quine.
+*/
+
+void describeFile()
+{
+    printf("coucou%s", "\n\\");
+//     printf("\
+// // 42 header %s
+// // 42 header\n\
+// \n\
+// #include <stdio.h>\n\
+// \n\
+// /*\n\
+//     This is the first program of Dr Quine.\n\
+// *\n\
+// \n\
+// void decribeFile()\n\
+// {\n\
+//     printf(\"write again\");\n\
+// }\n\
+// \n\
+// int main()\n\
+// {\n\
+//     /*\n\
+//         First Program: main.\n\
+//     */\n\
+//     describeFile();\n\
+//     return(0);\n\
+// }\n\
+// ", "\n\\");
+}
+
+int main() 
+{
+    /*
+        First Program: main.
+    */
+    describeFile();
+    return(0);
+}

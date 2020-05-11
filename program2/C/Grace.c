c87ce79414f67e524079bcf0b5ed40409f2f3df6
Aelaïg Weiler <aelaig.weiler@teamwork.net>
2020-05-11 15:53:11 +0200

    Grace

---
+++ program2/C/Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+    comment
+*/
+#define FILE_PATH "./Grace_kid.c"
+#define PROGRAM "#include <stdio.h>%2$c/*%2$c    comment%2$c*/%2$c#define FILE_PATH %3$c./Grace_kid.c%3$c%2$c#define PROGRAM %3$c%1$s%3$c%2$c#define MAIN() int main() {FILE *fp; fp = fopen(FILE_PATH, %3$cw+%3$c); fprintf(fp, PROGRAM, PROGRAM, 10, 34); fclose(fp); return(0);}%2$cMAIN();"
+#define MAIN() int main() {FILE *fp; fp = fopen(FILE_PATH, "w+"); fprintf(fp, PROGRAM, PROGRAM, 10, 34); fclose(fp); return(0);}
+MAIN();

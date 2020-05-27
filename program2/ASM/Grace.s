8f0673e44481e1fcbf0f44f38cb0f3dc440cd559
Aelaïg Weiler <aelaig.weiler@teamwork.net>
2020-05-27 15:03:19 +0200

    Grace ASM: correct if fp NULL

--- program2/ASM/Grace.s
+++ program2/ASM/Grace.s
@@ -11,9 +11,14 @@
 
         createFile %1, %2
 
+        cmp     r12, 0      ; check if file pointer is not null
+        je     _close_and_return
+
         writeFile %3, %4, %5, %6
 
-        call    _fclose         ; rdi already initiate with fd
+    _close_and_return:
+        mov     rdi, r12
+        call    _fclose
 
         pop     rbx
         ret
@@ -24,10 +29,12 @@
     mov     rsi, status
 
     call    _fopen
+
+    mov     r12, rax
 %endmacro
 
 %macro writeFile 4
-    mov     rdi, rax
+    mov     rdi, r12
     mov     rsi, %1
     mov     rdx, %1
     mov     rcx, %2
@@ -41,7 +48,7 @@ main file, status, format, arg1, arg2, arg3
 
     section .data
 format:
-    db      "    global _main%2$s    extern _fprintf%2$s    extern _fopen%2$s    extern _fclose%2$s%2$s    section .text%2$s%2$s%4$smacro main 6%2$s    _main:%2$s        push    rbx%2$s%2$s        createFile %4$s1, %4$s2%2$s%2$s        writeFile %4$s3, %4$s4, %4$s5, %4$s6%2$s%2$s        call    _fclose         ; rdi already initiate with fd%2$s%2$s        pop     rbx%2$s        ret%2$s%4$sendmacro%2$s%2$s%4$smacro createFile 2%2$s    mov     rdi, file%2$s    mov     rsi, status%2$s%2$s    call    _fopen%2$s%4$sendmacro%2$s%2$s%4$smacro writeFile 4%2$s    mov     rdi, rax%2$s    mov     rsi, %4$s1%2$s    mov     rdx, %4$s1%2$s    mov     rcx, %4$s2%2$s    mov     r8, %4$s3%2$s    mov     r9, %4$s4%2$s%2$s    call    _fprintf%2$s%4$sendmacro%2$s%2$smain file, status, format, arg1, arg2, arg3%2$s%2$s    section .data%2$sformat:%2$s    db      %3$s%1$s%3$s, 0%2$sarg1:%2$s    db      10, 0%2$sarg2:%2$s    db      34, 0%2$sarg3:%2$s    db      37, 0%2$sfile:%2$s    db      %3$sGrace_kid.s%3$s, 0%2$sstatus:%2$s    db      %3$sw+%3$s, 0", 0
+    db      "    global _main%2$s    extern _fprintf%2$s    extern _fopen%2$s    extern _fclose%2$s%2$s    section .text%2$s%2$s%4$smacro main 6%2$s    _main:%2$s        push    rbx%2$s%2$s        createFile %4$s1, %4$s2%2$s%2$s        cmp     r12, 0      ; check if file pointer is not null%2$s        je     _close_and_return%2$s%2$s        writeFile %4$s3, %4$s4, %4$s5, %4$s6%2$s%2$s    _close_and_return:%2$s        mov     rdi, r12%2$s        call    _fclose%2$s%2$s        pop     rbx%2$s        ret%2$s%4$sendmacro%2$s%2$s%4$smacro createFile 2%2$s    mov     rdi, file%2$s    mov     rsi, status%2$s%2$s    call    _fopen%2$s%2$s    mov     r12, rax%2$s%4$sendmacro%2$s%2$s%4$smacro writeFile 4%2$s    mov     rdi, r12%2$s    mov     rsi, %4$s1%2$s    mov     rdx, %4$s1%2$s    mov     rcx, %4$s2%2$s    mov     r8, %4$s3%2$s    mov     r9, %4$s4%2$s%2$s    call    _fprintf%2$s%4$sendmacro%2$s%2$smain file, status, format, arg1, arg2, arg3%2$s%2$s    section .data%2$sformat:%2$s    db      %3$s%1$s%3$s, 0%2$sarg1:%2$s    db      10, 0%2$sarg2:%2$s    db      34, 0%2$sarg3:%2$s    db      37, 0%2$sfile:%2$s    db      %3$sGrace_kid.s%3$s, 0%2$sstatus:%2$s    db      %3$sw+%3$s, 0", 0
 arg1:
     db      10, 0
 arg2:

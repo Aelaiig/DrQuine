b326a1ea61e0585bd35e1c2f8cd0f0732d7d9d8b
Aelaïg Weiler <aelaig.weiler@teamwork.net>
2020-05-28 10:32:43 +0200

    Sully ASM: correct if fp NULL

--- program3/ASM/Sully.s
+++ program3/ASM/Sully.s
@@ -25,7 +25,7 @@ _end_if_checkFile:
 
 _if_positif:
     cmp    rbx, -1
-    jng    _end_if_positif
+    jng    _end
 
 _concatenate_file:
     mov     rdi, 19
@@ -64,6 +64,8 @@ _createFile:
 
     call    _fopen
 
+    cmp     rax, 0
+    je      _end
     mov     rbp, rax
 
     mov     rdi, rbp
@@ -93,13 +95,13 @@ _concatenate_command:
     mov     rdi, r15
     call    _system
 
-_end_if_positif:
+_end:
     pop     rbx
     ret
 
         section .data
 format:
-    db      "    global  _main%2$s    extern  _fprintf%2$s    extern  _sprintf%2$s    extern  _malloc%2$s    extern  _fopen%2$s    extern  _fclose%2$s    extern  _system%2$s%2$s    section .text%2$s_main:%2$s    push    rbx%2$s%2$s    mov     rbx, %4$d%2$s%2$s_if_checkFile:%2$s    mov     rdi, fileSource%2$s    mov     rsi, currentFile%2$s    mov     rcx, currentFileLen + 1%2$s    cld                                 ; put df to 0, needed for string instruction%2$s    repe    cmpsb                       ;repeat until ecx = 0 compare byte address (e/r)di (e/r)si%2$s    jecxz   _end_if_checkFile           ; jump if ecx = 0%2$s_else_checkFile:%2$s    dec     rbx%2$s_end_if_checkFile:%2$s%2$s_if_positif:%2$s    cmp    rbx, -1%2$s    jng    _end_if_positif%2$s%2$s_concatenate_file:%2$s    mov     rdi, 19%2$s    call    _malloc%2$s%2$s    mov     r12, rax%2$s%2$s    mov     rdi, r12%2$s    mov     rsi, fileName%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s    mov     rdi, 19%2$s    call    _malloc%2$s%2$s    mov     r13, rax%2$s%2$s    mov     rdi, r13%2$s    mov     rsi, fileObject%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s    mov     rdi, 17%2$s    call    _malloc%2$s%2$s    mov     r14, rax%2$s%2$s    mov     rdi, r14%2$s    mov     rsi, executableName%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s_createFile:%2$s    mov     rdi, r12%2$s    mov     rsi, status%2$s%2$s    call    _fopen%2$s%2$s    mov     rbp, rax%2$s%2$s    mov     rdi, rbp%2$s    mov     rsi, format%2$s    mov     rdx, format%2$s    mov     rcx, arg1%2$s    mov     r8, arg2%2$s    mov     r9, rbx%2$s    call    _fprintf%2$s%2$s    mov     rdi, rbp%2$s    call    _fclose%2$s%2$s_concatenate_command:%2$s    mov     rdi, 200%2$s    call    _malloc%2$s%2$s    mov     r15, rax%2$s%2$s    mov     rdi, r15%2$s    mov     rsi, command%2$s    mov     rdx, r12%2$s    mov     rcx, r13%2$s    mov     r8, r14%2$s    call    _sprintf%2$s%2$s    mov     rdi, r15%2$s    call    _system%2$s%2$s_end_if_positif:%2$s    pop     rbx%2$s    ret%2$s%2$s        section .data%2$sformat:%2$s    db      %3$s%1$s%3$s, 0%2$sarg1:%2$s    db      10, 0%2$sarg2:%2$s    db      34, 0%2$sfileName:%2$s    db      %3$sSully_%%d.s%3$s, 0%2$sfileObject:%2$s    db      %3$sSully_%%d.o%3$s, 0%2$sexecutableName:%2$s    db      %3$sSully_%%d%3$s, 0%2$sstatus:%2$s    db      %3$sw+%3$s, 0%2$scommand:%2$s    db      %3$snasm -f macho64 %%1$s; ld -macosx_version_min 14.6.0 -lSystem -no_pie -o %%3$s %%2$s; rm %%2$s; ./%%3$s%3$s, 0%2$sfileSource:%2$s    db      %3$sSully.s%3$s, 0%2$scurrentFile:%2$s    db      __FILE__, 0%2$scurrentFileLen: equ     $-currentFile", 0
+    db      "    global  _main%2$s    extern  _fprintf%2$s    extern  _sprintf%2$s    extern  _malloc%2$s    extern  _fopen%2$s    extern  _fclose%2$s    extern  _system%2$s%2$s    section .text%2$s_main:%2$s    push    rbx%2$s%2$s    mov     rbx, %4$d%2$s%2$s_if_checkFile:%2$s    mov     rdi, fileSource%2$s    mov     rsi, currentFile%2$s    mov     rcx, currentFileLen + 1%2$s    cld                                 ; put df to 0, needed for string instruction%2$s    repe    cmpsb                       ;repeat until ecx = 0 compare byte address (e/r)di (e/r)si%2$s    jecxz   _end_if_checkFile           ; jump if ecx = 0%2$s_else_checkFile:%2$s    dec     rbx%2$s_end_if_checkFile:%2$s%2$s_if_positif:%2$s    cmp    rbx, -1%2$s    jng    _end%2$s%2$s_concatenate_file:%2$s    mov     rdi, 19%2$s    call    _malloc%2$s%2$s    mov     r12, rax%2$s%2$s    mov     rdi, r12%2$s    mov     rsi, fileName%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s    mov     rdi, 19%2$s    call    _malloc%2$s%2$s    mov     r13, rax%2$s%2$s    mov     rdi, r13%2$s    mov     rsi, fileObject%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s    mov     rdi, 17%2$s    call    _malloc%2$s%2$s    mov     r14, rax%2$s%2$s    mov     rdi, r14%2$s    mov     rsi, executableName%2$s    mov     rdx, rbx%2$s    call    _sprintf%2$s%2$s_createFile:%2$s    mov     rdi, r12%2$s    mov     rsi, status%2$s%2$s    call    _fopen%2$s%2$s    cmp     rax, 0%2$s    je      _end%2$s    mov     rbp, rax%2$s%2$s    mov     rdi, rbp%2$s    mov     rsi, format%2$s    mov     rdx, format%2$s    mov     rcx, arg1%2$s    mov     r8, arg2%2$s    mov     r9, rbx%2$s    call    _fprintf%2$s%2$s    mov     rdi, rbp%2$s    call    _fclose%2$s%2$s_concatenate_command:%2$s    mov     rdi, 200%2$s    call    _malloc%2$s%2$s    mov     r15, rax%2$s%2$s    mov     rdi, r15%2$s    mov     rsi, command%2$s    mov     rdx, r12%2$s    mov     rcx, r13%2$s    mov     r8, r14%2$s    call    _sprintf%2$s%2$s    mov     rdi, r15%2$s    call    _system%2$s%2$s_end:%2$s    pop     rbx%2$s    ret%2$s%2$s        section .data%2$sformat:%2$s    db      %3$s%1$s%3$s, 0%2$sarg1:%2$s    db      10, 0%2$sarg2:%2$s    db      34, 0%2$sfileName:%2$s    db      %3$sSully_%%d.s%3$s, 0%2$sfileObject:%2$s    db      %3$sSully_%%d.o%3$s, 0%2$sexecutableName:%2$s    db      %3$sSully_%%d%3$s, 0%2$sstatus:%2$s    db      %3$sw+%3$s, 0%2$scommand:%2$s    db      %3$snasm -f macho64 %%1$s; ld -macosx_version_min 14.6.0 -lSystem -no_pie -o %%3$s %%2$s; rm %%2$s; ./%%3$s%3$s, 0%2$sfileSource:%2$s    db      %3$sSully.s%3$s, 0%2$scurrentFile:%2$s    db      __FILE__, 0%2$scurrentFileLen: equ     $-currentFile", 0
 arg1:
     db      10, 0
 arg2:

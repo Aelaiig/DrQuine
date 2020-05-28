7578a9d67b7ec82b026902b2d3c61ca1c1c69474
Aelaïg Weiler <aelaig.weiler@teamwork.net>
2020-05-28 15:07:38 +0200

    correct node version script

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
 source $HOME/.brewconfig.zsh
 brew install nasm
-brew install node14 # node --version $> 14.3.0
+brew install node@14 # node --version $> 14.3.0
